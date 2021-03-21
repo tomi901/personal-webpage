0482078e41bb2212bdf247f872e5f956ff999936
Tomás Rinaldi <tomirinaldi8@gmail.com>
2021-03-21 14:42:26 -0300

    Added link buttons, responsive depending on screen width

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import './App.scss';
 import picture from './picture.png';
 
+
+
 function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+      <AppContent />
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+
+function AppHeader() {
   const particles: JSX.Element[] = [];
   for (let i = 0; i < 300; i++) {
     particles.push(<div className="particle" key={i}></div>);
   }
 
-  return (
-    <div className="App">
-      <header>
+  return <header>
         <div className="particle-container">{particles}</div>
         <div className="content">
           <img src={picture} className="picture" alt="Tomás Rinaldi's avatar" />
@@ -18,13 +30,24 @@ function App() {
           <h2>Developer</h2>
         </div>
         <p className="see-more">↓ Scroll down to see more ↓</p>
-      </header>
-      <section className="App-content">
-      </section>
-      <footer>
-      </footer>
-    </div>
-  );
+  </header>
 }
+
+function AppContent() {
+  const links: {
+    displayName: string;
+    link: string;
+  }[] = [
+    { displayName: "Curriculum Vitae", link: "https://firebasestorage.googleapis.com/v0/b/tom-rinaldi.appspot.com/o/public%2FWeb%2FCV.pdf?alt=media" },
+    { displayName: "LinkedIn", link: "https://www.linkedin.com/in/tom%C3%A1s-rinaldi-50244a1a1/" },
+  ];
+
+  return <section className="App-content">
+    <div className="btn-array">
+      {links.map((v, i) => <a key={i} className="btn" href={v.link} rel="noreferrer" target="_blank">{v.displayName}</a>)}
+    </div>
+  </section>
+}
+
 
 export default App;
