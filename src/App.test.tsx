13f0bdc1526a6976c4244eb44ed2dc1f90009861
Tomás Rinaldi <tomirinaldi8@gmail.com>
2021-03-27 14:06:49 -0300

    Moved app stuff to Home page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App, { AppHeader } from './App';
+import App from './App';
 
 
-test('Particles exist', () => {
-  const header = render(<AppHeader />);
-  const particlesContainers = header.baseElement.getElementsByClassName("particle-container");
-  expect(particlesContainers.length).toBeGreaterThan(0);
 
-  const particles = particlesContainers[0].getElementsByClassName("particle");
-  expect(particles.length).toBeGreaterThan(10);
-});
 
