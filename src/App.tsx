cd5c5c31531145f56e720d42df243f8792708bbd
Tomás Rinaldi <tomirinaldi8@gmail.com>
2021-04-06 13:58:55 -0300

    Added retro page for fun

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={React.lazy(() => import('pages/Home'))} />
           <Route path="/career" component={React.lazy(() => import('pages/Career'))} />
+          <Route path="/hypnospace" component={React.lazy(() => import('pages/Hypnospace'))} />
           <Route component={React.lazy(() => import('pages/NotFound'))} />
         </Switch>
       </React.Suspense>
